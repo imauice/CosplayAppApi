235bd824136e97b322d6cc63fa7e03444d8462c7
writen <sarnowsky94@gmail.com>
2021-10-04 17:36:30 +0200

    Delete comments

--- src/api/user/user-entity.ts
+++ src/api/user/user-entity.ts
@@ -6,19 +6,15 @@ import { authorize, genericController, preSave, val } from 'plumier';
 import { EntityBase } from '../_shared/entity-base';
 
 @genericController((c) => {
-    // POST /users accessible by public
     c.post().authorize('Public');
-
-    // PUT PATCH DELETE GET /users/{id} only accessible by the user itself
-    // See the user-policy.ts file how ResourceOwner defined
-    c.methods('Delete', 'GetOne', 'Patch', 'Put').authorize('ResourceOwner', 'Admin');
-
-    // GET /users?limit&offset&filter only accessible by Admin
+    c.methods('Delete', 'GetOne', 'Patch', 'Put').authorize(
+        'ResourceOwner',
+        'Admin'
+    );
     c.getMany().authorize('Admin');
 })
 @collection()
 export class User extends EntityBase {
-    // email will only visible by the user itself or by Admin
     @authorize.read('ResourceOwner', 'Admin')
     @val.required()
     @val.unique()
@@ -28,15 +24,12 @@ export class User extends EntityBase {
     @collection.ref((x) => Profile)
     profile: Ref<Profile>;
 
-    // password will not visible to anyone
     @authorize.writeonly()
     @val.required()
     password: string;
 
     @val.enums(['Admin', 'User'])
-    // role only can be set by Admin
     @authorize.write('Admin')
-    // role only visible to the user itself or by Admin
     @authorize.read('ResourceOwner', 'Admin')
     @collection.property({ default: 'User' })
     role: 'User' | 'Admin';
@@ -48,7 +41,11 @@ export class User extends EntityBase {
 
     @preSave()
     async initEntity() {
-        if (this.password) this.password = await bcrypt.hash(this.password, await bcrypt.genSalt());
+        if (this.password)
+            this.password = await bcrypt.hash(
+                this.password,
+                await bcrypt.genSalt()
+            );
     }
 }
 
